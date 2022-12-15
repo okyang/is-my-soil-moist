61d58796147935b40740f6057b620b7db5feb6d3
okyang <owenyang123@gmail.com>
2022-12-14 22:59:43 -0800

    add wio terminal platformio code

---
+++ soil_moisture_terminal/src/main.cpp
@@ -0,0 +1,140 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <rpcWiFi.h>
+#include <SPI.h>
+
+#include "config.h"
+
+WiFiClient wioClient;
+PubSubClient client(wioClient);
+int moisture_adc;
+char buffer[40];
+
+/// @brief connect wio terminal to wifi
+void connectWiFi()
+{
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("Connecting to WiFi..");
+        WiFi.begin(settings::SSID, settings::PASSWORD);
+        delay(500);
+    }
+
+    Serial.println("Connected!");
+}
+
+/// @brief function to call whenever `client.loop` is run
+/// @param topic the MQTT topic
+/// @param payload the value to send to the topic
+/// @param length length of the payload
+void clientCallback(char *topic, uint8_t *payload, unsigned int length)
+{
+    char buff[length + 1];
+    for (unsigned int i = 0; i < length; i++)
+    {
+        buff[i] = (char)payload[i];
+    }
+    buff[length] = '\0';
+
+    Serial.print("Message received:");
+    Serial.println(buff);
+
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, buff);
+    JsonObject obj = doc.as<JsonObject>();
+
+    bool relay_on = obj["relay_on"];
+
+    if (relay_on)
+        digitalWrite(PIN_WIRE_SCL, HIGH);
+    else
+        digitalWrite(PIN_WIRE_SCL, LOW);
+}
+
+/// @brief connects to MQTT broker if disconnected or not connected at all
+void reconnectMQTTClient()
+{
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+
+        if (client.connect(settings::CLIENT_NAME.c_str()))
+        {
+            Serial.println("connected");
+            client.subscribe(settings::SERVER_COMMAND_TOPIC.c_str());
+        }
+        else
+        {
+            Serial.print("Retying in 5 seconds - failed, rc=");
+            Serial.println(client.state());
+
+            delay(5000);
+        }
+    }
+}
+
+/// @brief Setup connection to MQTT broker. Also uses the `clientCallback` as the
+///        callback function (the function called every iteration of of `client.loop()).
+void createMQTTClient()
+{
+    client.setServer(settings::BROKER.c_str(), 1883);
+    client.setCallback(clientCallback);
+    reconnectMQTTClient();
+}
+
+/// @brief converts the raw ADC values from the moisture sensor readings to a %.
+///        You'll need to specify the MOISTURE_ADC_MIN and MOISTURE_ADC_MAX values
+///        in your config.h file.
+/// @param moisture_adc the raw adc value from the moisture sensor reading
+/// @return the percentage of how moisture. 0% is dry. 100% is completely wet.
+int adcMoistureToPercent(int moisture_adc)
+{
+    if (moisture_adc <= settings::MOISTURE_ADC_MIN)
+    {
+        return 0;
+    }
+    else if (moisture_adc >= settings::MOISTURE_ADC_MAX)
+    {
+        return 100;
+    }
+    else
+    {
+        return 100 * (moisture_adc - settings::MOISTURE_ADC_MIN) / (settings::MOISTURE_ADC_MAX - settings::MOISTURE_ADC_MIN);
+    }
+    return 10;
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // pin setup
+    pinMode(A0, INPUT);
+    pinMode(PIN_WIRE_SCL, OUTPUT);
+
+    // mqtt client setup
+    connectWiFi();
+    createMQTTClient();
+}
+
+void loop()
+{
+    reconnectMQTTClient();
+    client.loop();
+
+    moisture_adc = analogRead(A0);
+
+    DynamicJsonDocument doc(1024);
+    doc["soil_moisture"] = adcMoistureToPercent(moisture_adc);
+
+    std::string telemetry;
+    serializeJson(doc, telemetry);
+
+    Serial.print("Sending telemetry ");
+    Serial.println(telemetry.c_str());
+    Serial.println(settings::CLIENT_TELEMETRY_TOPIC.c_str());
+    client.publish(settings::CLIENT_TELEMETRY_TOPIC.c_str(), telemetry.c_str());
+
+    delay(10000); // 10 seconds
+}
---
+++ soil_moisture_terminal/src/template.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+
+namespace settings {
+    // WiFi credentials
+    const char *SSID = "<YOUR_WIFI_ID>";
+    const char *PASSWORD = "<YOUR_WIFI_PASSWORD>";
+
+    // you should calibrate these values
+    const int MOISTURE_ADC_MIN = 2;        // adc value when the moisture sensor is completely dry
+    const int MOISTURE_ADC_MAX =  700;     // adc value when the moisture sensor is completely wet
+
+    // MQTT settings
+    // create a GUID with a tool like https://guidgenerator.com/
+    // we use this because we are using a public MQTT broker
+    // adding this GUID ensures that we have a unique MQTT topic.
+    const std::string ID = "<YOUR_GUID>";
+
+    const std::string BROKER = "broker.hivemq.com"; //"test.mosquitto.org";
+    const std::string CLIENT_NAME = ID + "soilmoisturesensor_client";
+
+    const std::string CLIENT_TELEMETRY_TOPIC = ID + "/telemetry";
+    const std::string SERVER_COMMAND_TOPIC = ID + "/commands";
+
+}
